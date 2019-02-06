f2365a2836688787b4b966961e6577dc62489761
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2019-02-06 09:26:18 -0700

    Added Entry File

---
+++ Journal_1_/Model/Entry.swift
@@ -0,0 +1,13 @@
+//
+//  Entry.swift
+//  Journal_1_
+//
+//  Created by Taylor Bills on 2/6/19.
+//  Copyright © 2019 Taylor Bills. All rights reserved.
+//
+
+import Foundation
+
+class Entry {
+    
+}
